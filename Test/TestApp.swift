642a20efaa4ca572c19b972d7609749167d7f012
devpnm <devpnm@icloud.com>
2024-03-08 11:17:52 -0500

    Init

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Nicolas Makovnik on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
